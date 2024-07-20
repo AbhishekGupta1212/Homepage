d60f42ac888ed4ca5abfb353992b76adb9586cbc
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2024-07-20 14:04:06 +0530

    done

--- homepage/src/Components/FeaturedCollection/FeaturedCollection.jsx
+++ homepage/src/Components/FeaturedCollection/FeaturedCollection.jsx
@@ -16,11 +16,11 @@ const FeaturedCollection = () => {
             styles and mediums from renowned and emerging artists."
           </p>
           <button className={styles.ctaButton}>Buy Now</button>
-          <img
+          {/* <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/c611442c8bbf7563843ba7fed51faa4979431228bbd6e102cae49ce1f824924c?apiKey=3d13fd40ad0c42f4bdd670568134c3ca&"
             alt=""
             className={styles.decorativeImage}
-          />
+          /> */}
         </div>
         <div className={styles.imageWrapper}>
           <img
@@ -40,17 +40,17 @@ const FeaturedCollection = () => {
             alt="Additional Artwork 1"
             className={styles.additionalImage1}
           />
-          {/* <img
+          <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/3417a034a508960e659642d21622f85c94393e9409967c8b2ee953be6340fba1?apiKey=3d13fd40ad0c42f4bdd670568134c3ca&"
             alt="Additional Artwork 2"
             className={styles.additionalImage2}
-          /> */}
-          <div className={styles.imageDots}>
+          />
+          {/* <div className={styles.imageDots}>
             <span className={styles.dot} />
             <span className={styles.dot} />
             <span className={styles.dot} />
             <span className={styles.dot} />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
